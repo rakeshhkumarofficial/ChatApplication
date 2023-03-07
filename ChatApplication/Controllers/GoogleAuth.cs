e67f0ac9eac7997425aabaddf8f42ab5c71d3889
Rakesh Kumar <gpaswan1976@gmail.com>
2023-03-07 19:27:29 +0530

    Google Auth added

---
+++ ChatApplication/Controllers/GoogleAuth.cs
@@ -0,0 +1,61 @@
+﻿using ChatApplication.Data;
+using ChatApplication.Models;
+using ChatApplication.Services;
+using Google.Apis.Auth;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace ChatApplication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GoogleAuth : ControllerBase
+    {
+        private readonly ChatAPIDbContext _dbContext;
+        public readonly IConfiguration _configuration;
+
+        public GoogleAuth(ChatAPIDbContext dbContext, IConfiguration configuration)
+        {
+            _dbContext = dbContext;
+            _configuration = configuration;
+
+        }
+
+
+        [HttpPost]
+        [Route("GoogleAuth")]
+        public async Task<IActionResult> SignIn(string Token)
+        {
+            var googleUser = await GoogleJsonWebSignature.ValidateAsync(Token);
+            var user = new GoogleUser();
+            user.Email = googleUser.Email;
+            user.Password = null;
+            string token = CreateToken(user, _configuration);
+            var response = new Response();
+            response.StatusCode = 200;
+            response.Message = "Token";
+            response.Data = token;
+            return Ok(response);
+        }
+        private string CreateToken(GoogleUser obj, IConfiguration _configuration)
+        {
+            List<Claim> claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name,obj.Email)
+           };
+            var Key = new Microsoft.IdentityModel.Tokens.SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(_configuration.GetSection("AppSettings:Token").Value));
+            var creds = new SigningCredentials(Key, SecurityAlgorithms.HmacSha512Signature);
+            var token = new JwtSecurityToken(
+                claims: claims,
+                expires: DateTime.Now.AddDays(1),
+                signingCredentials: creds
+            );
+            var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+            return jwt;
+        }
+    }
+}
