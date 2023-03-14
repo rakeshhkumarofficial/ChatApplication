64c11301faefd1f7d6d05d47ef42d3d50f293060
Rakesh Kumar <gpaswan1976@gmail.com>
2023-03-14 17:33:14 +0530

    created chathub and made the connection to frontend

--- ChatApplication/Controllers/ChatController.cs
+++ ChatApplication/Controllers/ChatController.cs
@@ -1,46 +1,99 @@
-﻿using ChatApplication.Data;
-using ChatApplication.Models;
-using ChatApplication.Services;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Security.Claims;
+﻿//using ChatApplication.Data;
+//using ChatApplication.Hubs;
+//using ChatApplication.Models;
+//using ChatApplication.Services;
+//using Microsoft.AspNetCore.Authorization;
+//using Microsoft.AspNetCore.Http;
+//using Microsoft.AspNetCore.Mvc;
+//using Microsoft.AspNetCore.SignalR;
+//using System;
+//using System.Security.Claims;
 
-namespace ChatApplication.Controllers
-{
-    [Route("api/[controller]/[Action]")]
-    [ApiController]
-    public class ChatController : ControllerBase
-    {
-        private readonly ChatAPIDbContext _dbContext;
-        public readonly IConfiguration _configuration;
-        public ChatController(ChatAPIDbContext dbContext, IConfiguration configuration)
-        {
-            _dbContext = dbContext;
-            _configuration = configuration;
+//namespace ChatApplication.Controllers
+//{
+//    [Route("api/[controller]/[Action]")]
+//    [ApiController]
+//    public class ChatController : ControllerBase
+//    {
 
-        }
+//        private readonly IHubContext<ChatHub> _hubContext;
 
-        [HttpPost,Authorize(Roles ="Login")]
-        public IActionResult ChatRoom(ChatRequest chat)
-        {
-            var user = HttpContext.User;
-            var email = user.FindFirst(ClaimTypes.Name)?.Value;
-            var dbuser = _dbContext.Users.FirstOrDefault(x => x.Email == email);
-            IChatService chatService = new ChatService(_dbContext,_configuration);
-            var res = chatService.ChatRoom( chat.UserId, dbuser.UserId);
-            return Ok(res);          
-        }
+//        public ChatController(IHubContext<ChatHub> hubContext)
+//        {
+//            _hubContext = hubContext;
+//        }
+                                         
+//        [HttpPost]
+//        public IActionResult Send(Guid ReceiverId , string message)
+//        {
+//            _hubContext.Clients.All.SendAsync("ReceiveMessage", ReceiverId, message);
+//            return Ok();
+//        }
 
-        [HttpDelete,Authorize(Roles ="Login")]
-        public IActionResult DeleteRoom(Guid roomId)
-        {
-            var user = HttpContext.User;
-            var email = user.FindFirst(ClaimTypes.Name)?.Value;  
-            IChatService chatService = new ChatService(_dbContext, _configuration);
-            var res = chatService.DeleteRoom(email,roomId);
-            return Ok(res);
-        }
-    }
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//        /*
+//        private readonly ChatAPIDbContext _dbContext;
+//        public readonly IConfiguration _configuration;
+//        public ChatController(ChatAPIDbContext dbContext, IConfiguration configuration)
+//        {
+//            _dbContext = dbContext;
+//            _configuration = configuration;
+
+//        }
+
+//        [HttpPost,Authorize(Roles ="Login")]
+//        public IActionResult ChatRoom(ChatRequest chat)
+//        {
+//            var user = HttpContext.User;
+//            var email = user.FindFirst(ClaimTypes.Name)?.Value;
+//            var dbuser = _dbContext.Users.FirstOrDefault(x => x.Email == email);
+//            IChatService chatService = new ChatService(_dbContext,_configuration);
+//            var res = chatService.ChatRoom( chat.UserId, dbuser.UserId);
+//            return Ok(res);          
+//        }
+
+//        [HttpDelete,Authorize(Roles ="Login")]
+//        public IActionResult DeleteRoom(Guid roomId)
+//        {
+//            var user = HttpContext.User;
+//            var email = user.FindFirst(ClaimTypes.Name)?.Value;  
+//            IChatService chatService = new ChatService(_dbContext, _configuration);
+//            var res = chatService.DeleteRoom(email,roomId);
+//            return Ok(res);
+//        }
+//        */
+//    }
+//}
--- ChatApplication/Hubs/ChatHub.cs
+++ ChatApplication/Hubs/ChatHub.cs
@@ -2,6 +2,7 @@
 using ChatApplication.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.SignalR;
+using System.Security.Claims;
 
 namespace ChatApplication.Hubs
 {
@@ -12,23 +13,30 @@ namespace ChatApplication.Hubs
         {
             _dbContext = dbContext;
         }
-        [Authorize(Roles ="Login")]
-        public async Task SendMessageToReciever(Guid SenderId, Guid RecieverId, string message)
+       
+
+       // [Authorize(Roles ="Login")]
+        public async Task SendMessage(Guid ReceiverId, string message)
          {
+            var httpContext = Context.GetHttpContext();
+            var user1 = httpContext.User;
+            var email = user1.FindFirst(ClaimTypes.Name)?.Value;  
+            var User = _dbContext.Users.FirstOrDefault(u=>u.Email== email);
             var msg = new ChatMessage()
             {
                 MessageId = Guid.NewGuid(),
-                SenderId = SenderId,
-                RecieverId = RecieverId,
+                SenderId = User.UserId,
+                RecieverId = ReceiverId,
                 Message = message,
                 TimeStamp = DateTime.UtcNow
-     
             };
-            var Sender = from u in _dbContext.Users where u.UserId == SenderId select u.FirstName;         
+            var Sender = from u in _dbContext.Users where u.Email == email select new {u.FirstName, u.LastName};         
             _dbContext.ChatMessages.Add(msg);
             _dbContext.SaveChanges();
             
-            await Clients.User(RecieverId.ToString()).SendAsync("ReceiveMessage", Sender, message);
+            await Clients.User(ReceiverId.ToString()).SendAsync("ReceiveMessage", Sender, message, Context.ConnectionId);
          }
+        
+       
     }
 }
